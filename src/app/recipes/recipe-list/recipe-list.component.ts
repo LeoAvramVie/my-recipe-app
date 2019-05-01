372c14b4122d6e098416d2ba2901fb8072c23b19
leo.avramovic@gmail.com <keksirulez1>
2019-05-02 00:16:42 +0200

    recipe-list

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is a simply test',
+      'https://commons.wikimedia.org/wiki/File:Recipe_logo.jpeg'),
+    new Recipe(
+      'A 2nd Test Recipe',
+      'This is a simply 2nd test',
+      'https://commons.wikimedia.org/wiki/File:Recipe_logo.jpeg'),
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
