4d7b145b472700784bcd8698a07daef7afc9a805
Leo Avramovic <leo.avramovic@gmail.com>
2019-06-29 21:32:49 +0200

    299. Attaching the Token with an Interceptor

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,8 +1,9 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {Recipe} from '../recipes/recipe.model';
 import {RecipeService} from '../recipes/recipe.service';
-import {map, tap} from 'rxjs/operators';
+import {exhaustMap, map, take, tap} from 'rxjs/operators';
+import {AuthService} from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,8 @@ import {map, tap} from 'rxjs/operators';
 export class DataStorageService {
 
   constructor(private http: HttpClient,
-              private recipeService: RecipeService) {
+              private recipeService: RecipeService,
+              private authService: AuthService) {
   }
 
   storeRecipes() {
@@ -19,11 +21,13 @@ export class DataStorageService {
       .subscribe(response => {
         console.log(response);
       });
-
   }
 
   fetchRecipes() {
-    return this.http.get<Recipe[]>('https://ng-myrecipebook.firebaseio.com/recipes.json')
+    return this.http
+      .get<Recipe[]>(
+          'https://ng-myrecipebook.firebaseio.com/recipes.json'
+        )
       .pipe(
         map(recipes => {
           return recipes.map(recipe => {
@@ -38,5 +42,4 @@ export class DataStorageService {
         })
       );
   }
-
 }
