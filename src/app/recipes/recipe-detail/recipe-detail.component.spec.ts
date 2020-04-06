3273cf676eb0739b8a8badf37519bf6b5255d9b7
LeoAvram <leo.avramovic@gmail.com>
2020-04-06 12:07:07 +0200

    Fixing security vulnerabilities in npm dependencies

--- src/app/recipes/recipe-detail/recipe-detail.component.spec.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.spec.ts
@@ -1,26 +1,6 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
-import {RecipeDetailComponent} from './recipe-detail.component';
-import {RecipeService} from '../recipe.service';
-import {Store} from '@ngrx/store';
-
-
 describe('Component: Recipe-Details', () => {
-  let component: RecipeDetailComponent;
-  let fixture : ComponentFixture<RecipeDetailComponent>;
-  let testBedService: RecipeService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [RecipeDetailComponent],
-      providers: [RecipeService, Store]
-    });
-    fixture = TestBed.createComponent(RecipeDetailComponent);
-    component = fixture.componentInstance;
-    testBedService = TestBed.get(RecipeDetailComponent);
+  beforeEach(()=>{
+    
   });
-
-  it('should create the component', () => {
-    expect(component).toBeTruthy();
-    });
   });
 
